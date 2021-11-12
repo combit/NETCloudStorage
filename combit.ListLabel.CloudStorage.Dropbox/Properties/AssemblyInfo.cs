09107d29838549fe1b3085d100884569d88caa99
Daniel Kenner <DanielKenner@users.noreply.github.com>
2021-11-12 14:37:52 +0100

    Release 27.000

--- combit.ListLabel.CloudStorage.Dropbox/Properties/AssemblyInfo.cs
+++ combit.ListLabel.CloudStorage.Dropbox/Properties/AssemblyInfo.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("combit List & Label 26 Dropbox Uploader (.NET Framework 4)")]
+[assembly: AssemblyTitle("combit List & Label 27 Dropbox Uploader (.NET Framework 4)")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("8135c009-7702-42eb-8d72-4b9130b52414")]
@@ -15,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © combit GmbH")]
 [assembly: AssemblyTrademark("combit and List & Label are registered trademarks of combit GmbH, Germany, www.combit.net")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("26.0.*")]
-[assembly: AssemblyFileVersion("26.0.0.0")]
+[assembly: AssemblyVersion("27.0.*")]
+[assembly: AssemblyFileVersion("27.0.0.0")]
--- combit.ListLabel.CloudStorage.MicrosoftOneDrive/Properties/AssemblyInfo.cs
+++ combit.ListLabel.CloudStorage.MicrosoftOneDrive/Properties/AssemblyInfo.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("combit List & Label 26 Microsoft OneDrive Uploader (.NET Framework 4)")]
+[assembly: AssemblyTitle("combit List & Label 27 Microsoft OneDrive Uploader (.NET Framework 4)")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("cefef3c0-c2a8-41a0-b79f-3f7555b066f6")]
@@ -15,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © combit GmbH")]
 [assembly: AssemblyTrademark("combit and List & Label are registered trademarks of combit GmbH, Germany, www.combit.net")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("26.0.*")]
-[assembly: AssemblyFileVersion("26.0.0.0")]
+[assembly: AssemblyVersion("27.0.*")]
+[assembly: AssemblyFileVersion("27.0.0.0")]
